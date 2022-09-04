801ef4cfa5db7e195119dd1be917407c331cf2e2
ilya-pankratov <112556602+ilya-pankratov@users.noreply.github.com>
2022-09-04 23:47:07 +0300

    .

--- Caesar 's Cipher.py
+++ Caesar 's Cipher.py
@@ -3,16 +3,16 @@ def is_valid_key(value):
     if value.isdigit() and int(value) > 0:
         return int(value)
     else:
-        return is_valid_key(input('Неккоректное значение. Попробуйте еще раз: '))
+        return is_valid_key(input('Некоректное значение. Попробуйте еще раз: '))
 
 
 def is_valid_language(value):
     if value.lower() == 'английский':
-        return 'английский'
+        return 'abcdefghijklmnopqrstuvwxyz'
     elif value.lower() == 'русский':
-        return 'русский'
+        return 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
     else:
-        is_valid_language(input('Неккоректное значение. Попробуйте еще раз: '))
+        is_valid_language(input('Некоректное значение. Попробуйте еще раз: '))
 
 
 def is_valid_code(value):
@@ -21,38 +21,33 @@ def is_valid_code(value):
     elif value.lower() == 'дешифрование':
         return 'дешифрование'
     else:
-        is_valid_code(input('Неккоректное значение. Попробуйте еще раз: '))
+        is_valid_code(input('Некоректное значение. Попробуйте еще раз: '))
 
 
-def encrypt(code, language, key, message):
+def encrypt(code, letters, key, message):
     if code == 'дешифрование':
         key = -key
-    if language == 'русский':
-        upper_letters = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-        lower_letters = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+    if language == 'абвгдежзийклмнопрстуфхцчшщъыьэюя':
         max = 32
-    if language == 'английский':
-        upper_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        lower_letters = 'abcdefghijklmnopqrstuvwxyz'
+    if language == 'abcdefghijklmnopqrstuvwxyz':
         max = 25
 
     for i in range(len(message)):
         if message[i].isalpha():
             if message[i] == message[i].upper():
-                index_old = upper_letters.find(message[i])
+                index_old = letters.upper().find(message[i])
                 index_new = index_old + key
                 if index_new > max:
                     index_new -= max + 1
-                message[i] = upper_letters[index_new]
+                message[i] = letters.upper()[index_new]
             else:
-                index_old = lower_letters.find(message[i])
+                index_old = letters.find(message[i])
                 index_new = index_old + key
                 if index_new > max:
                     index_new -= max + 1
-                message[i] = lower_letters[index_new]
+                message[i] = letters[index_new]
 
-    print('\nЗашифрованное сообщение:')
-    print(''.join(message))
+    return ''.join(message)
 
 
 code = is_valid_code(input('Выберите направление (шифрование/дешифрование): '))
@@ -60,5 +55,6 @@ language = is_valid_language(input('Выберите язык сообщения
 key = is_valid_key(input('Введите шаг сдвига: '))
 message = [i for i in input('Введите сообщение: ')]
 
-encrypt(code, language, key, message)
+print('\nЗашифрованное сообщение:')
+print(encrypt(code, language, key, message))
 
